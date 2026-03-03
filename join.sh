78ca434beff3747680db06df8461adad1aeffe27
Ez7011 <81989477+Ez7011@users.noreply.github.com>
2026-03-03 11:37:38 +0100

    Update join.sh

--- join.sh
+++ join.sh
@@ -5,7 +5,7 @@ mkdir -p ../Emma_im/
 
 path="/work/FAC/FBM/CIG/dgatfiel/smg6_kinnex/DG_kinnexmRNA_DEMUXED_Full_Length_Non_Concensus_HQ_transcript"
 
-EmmaPath="/work/FAC/FBM/CIG/dgatfiel/smg6_kinnex/Emma_im/"
+EmmaPath="/work/FAC/FBM/CIG/dgatfiel/smg6_kinnex/Emma_im"
 
 file1="collapse_isoforms.flnc_count.txt"
 file2="pigeon.classification.txt"
@@ -21,17 +21,19 @@ lines2=$(wc -l < "$path/$file2")
 echo "In $file1, there are $lines1 lines."
 echo "In $file2, there are $lines2 lines."
 
-sed 's/,/\t/g' /work/FAC/FBM/CIG/dgatfiel/smg6_kinnex/DG_kinnexmRNA_DEMUXED_Full_Length_Non_Concensus_HQ_transcript/collapse_isoforms.flnc_count.txt > /work/FAC/FBM/CIG/dgatfiel/smg6_kinnex/Emma_im/collapse_isoforms.flnc_count_tab.txt
+file3="collapse_isoforms.flnc_count_tab.txt"
 
-cut -f1 "$EmmaPath/$file1" > "$EmmaPath/flnc_ids.txt"
+sed 's/,/\t/g' "$EmmaPath/$file1" > "$EmmaPath/$file3"
+
+cut -f1 "$EmmaPath/$file3” > "$EmmaPath/flnc_ids.txt"
 cut -f1 "$EmmaPath/$file2" > "$EmmaPath/class_ids.txt"
-echo "PB IDs from reads and classifications are stored in 'flnc_ids.txt' and 'class_ids.txt'.”
+echo "PB IDs from reads and classifications are stored in 'flnc_ids.txt' and 'class_ids.txt'."
 
 comm -12 <(sort "$EmmaPath/flnc_ids.txt") <(sort "$EmmaPath/class_ids.txt") > "$EmmaPath/common_ids.txt"
 
 echo "two files are sorted and filtered by common PB IDs stored in 'common_ids.txt'."
 
-awk 'FNR==NR {ids[$1]; next} $1 in ids' "$EmmaPath/common_ids.txt" "$EmmaPath/$file1" > "$EmmaPath/filtered_flnc.txt" 
+awk 'FNR==NR {ids[$1]; next} $1 in ids' "$EmmaPath/common_ids.txt" "$EmmaPath/$file3” > "$EmmaPath/filtered_flnc.txt" 
 awk 'FNR==NR {ids[$1]; next} $1 in ids' "$EmmaPath/common_ids.txt" "$EmmaPath/$file2” > "$EmmaPath/filtered_class.txt" 
 
 echo "in classification and flnc, only lines that contains the common ids are extracted and stored in the filtered_class.txt and filtered_flnc.txt"
